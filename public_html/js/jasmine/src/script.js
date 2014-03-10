4ac607111892fce244ac8a8916fa3ed28ea67ac4
BobDole <ac.aaroncowan@gmail.com>
2014-03-10 17:38:07 -0300

    added a src and spec folder for jasmine testing

---
+++ public_html/js/jasmine/src/script.js
@@ -0,0 +1,50 @@
+function show(){
+document.getElementById("course_add").style.visibility="visible";
+document.getElementById("show_button").style.visibility="hidden";
+}
+	var counter=0;
+
+function addCourse(){
+	
+	var parentDiv = document.createElement("div");	//Create the blocks to display a course
+	var name = document.createElement("div");
+	var code = document.createElement("div");
+	var number = document.createElement("div");
+	var start = document.createElement("div");
+	var duration = document.createElement("div");
+	//var insert_after = document.createElement("div");
+	
+	name.setAttribute("id", "course_name");		//Set the "ID" attributes of the div elements
+	code.setAttribute("id", "course_code");
+	number.setAttribute("id", "course_number");
+	start.setAttribute("id", "course_start");
+	duration.setAttribute("id", "course_duration");
+	//insert_after.setAttribute("id", "insert_after");
+	counter=counter+1;
+	parentDiv.setAttribute("id", "course_"+counter);
+	
+	var form_name = document.getElementById("form_course_name");
+	
+	name.innerHTML = document.getElementById("form_course_name").value;
+	var form_course_code = document.getElementById("form_course_code");
+	code.innerHTML = form_course_code.options[form_course_code.selectedIndex].value;
+	//code.innerHTML = document.getElementById("form_course_code").value;
+	number.innerHTML = document.getElementById("form_course_number").value;
+	start.innerHTML = document.getElementById("form_course_start_time").value;
+	duration.innerHTML = document.getElementById("form_course_duration").value;
+	
+	
+	var insertCourse = document.getElementById("insert_here");	//Find where to display the course info in the html code
+	//insert.setAttribute("id", "course_"+counter);	//change the id of the element found. We are going to place another element with that id after the block with this course
+	
+	parentDiv.appendChild(name);
+	parentDiv.appendChild(code);
+	parentDiv.appendChild(number);
+	parentDiv.appendChild(start);
+	parentDiv.appendChild(duration);
+	//parentDiv.appendChild(insert_after);
+	//insertParent.insertBefore(insertCourse, parentDiv);
+	insertCourse.appendChild(parentDiv);
+	
+	//document.getElementById("course_name").innerHTML += "Test!";
+}
