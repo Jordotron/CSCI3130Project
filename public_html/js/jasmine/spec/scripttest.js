ce842fc68f29b1e8532186010c094889c3985acd
BobDole <ac.aaroncowan@gmail.com>
2014-03-12 19:13:01 -0300

    added specrunner and jasmine test

--- public_html/js/jasmine/spec/scripttest.js
+++ public_html/js/jasmine/spec/scripttest.js
@@ -1,1 +1,41 @@
 
+describe("Make sure counter > 0", function() {
+    it("Must be zero or greater amount of classes", function() {
+        expect(counter).toBeGreaterThan(-1);
+	});
+});
+
+
+describe("name div", function() {
+    it("check if div for name is being created", function() {
+		var parentDiv = document.createElement("div");
+        expect(parentDiv).toBeDefined();
+	});
+});
+/*
+describe("add course button", function() {
+		var form_course_add_button;
+		var name;
+		
+		beforeEach(function() {
+			form_course_add_button = jQuery('#course_add_button');
+		});
+		afterEach(function(){	
+			name = jQuery('#course_name');
+		});
+		
+		describe('add course name', function() {
+			it('should add a course name', function() {
+				form_course_add_button.click();
+				expect(name.text().length).toBeGreaterThan(0);
+			})
+		})
+});
+*//*
+describe("script",function(){
+	var addCourse = new addCourse();
+	it("calls the add course function", function() {		
+		expect(addCourse).toBeDefined();
+	});
+});
+	*/
